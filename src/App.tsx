13bf8235f4f079de7f69465fbb657c14f20ea4cb
crymfox <sameur.bh@gmail.com>
2022-11-25 22:12:09 -0800

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Route } from "react-router-dom"
+import { Route, Routes, redirect } from "react-router-dom"
 import Kscan from "./components/Kscan"
 import Home from "./components/Home"
 
 const App = () => {
   return (
     <div>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/kscan" element={<Kscan/>}/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/kscan" element={<Kscan/>}/>
+      </Routes>
     </div>
   )
 }
